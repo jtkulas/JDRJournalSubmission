ebe757f35a36042a44488794d94b697589d62613
John Kulas <jtkulas@ergreports.com>
2022-09-30 12:13:11 -0500

    ANOVA added

--- SIOP2023/Counts/graphing.R
+++ SIOP2023/Counts/graphing.R
@@ -28,3 +28,25 @@ rc$scale <- c(rep("Structural",10), rep("Physical", 20), rep("Interpersonal",13)
 ggplot(rc, aes(x = mean, fill = scale)) +
   geom_density(alpha = .3) +
   xlab("Percent Agreement") + labs(fill = "ONET grouping") 
+
+
+## Repeated measures ANOVA for resource_challenge
+
+anova <- data[,c(495:578)]
+
+anova$Structural <- rowSums(anova[1:10], na.rm=TRUE)
+anova$Physical <- rowSums(anova[11:30], na.rm=TRUE)
+anova$Interpersonal <- rowSums(anova[31:43], na.rm=TRUE)
+anova$Information <- rowSums(anova[44:48], na.rm=TRUE)
+anova$Mental <- rowSums(anova[49:58], na.rm=TRUE)
+anova$Output <- rowSums(anova[59:67], na.rm=TRUE)
+anova$Interact <- rowSums(anova[68:84], na.rm=TRUE)
+
+library(tidyr)
+
+anova <- cbind(person = 1:nrow(anova), anova)  ## creating person identifier for ANOVA
+
+analysis <- gather(anova, scale, value, Structural:Interact)
+
+repmeas <- aov(value ~ factor(scale) + Error(factor(person)), data=analysis)
+summary(repmeas)
